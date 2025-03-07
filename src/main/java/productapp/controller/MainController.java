8118a9f46e04562f31846ff2db5b0e56a64a4f6e
Vedant <vedant10vaidya@gmail.com>
2025-03-08 03:06:43 +0900

    Spring MVC | Product CRUD App | Add, Delete complete

--- src/main/java/productapp/controller/MainController.java
+++ src/main/java/productapp/controller/MainController.java
@@ -1,14 +1,31 @@
 package productapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import productapp.dao.ProductDao;
+import productapp.entities.Product;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private ProductDao productDao;
+	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
+		List<Product> products = productDao.getProducts();
+		model.addAttribute("products", products);
+		
 		return "index";
 	}
 	
@@ -16,6 +33,28 @@ public class MainController {
 	public String addProduct(Model model) {
 		model.addAttribute("title", "Add Product");
 		return "addProductForm";
+	}
+	
+	@RequestMapping(value = "/handle-product", method = RequestMethod.POST)
+	public RedirectView handleAddProduct(@ModelAttribute Product product,
+										 HttpServletRequest request) {
+		System.out.println(product);
+		productDao.createProduct(product);
+		RedirectView view = new RedirectView();
+		view.setUrl(request.getContextPath() + "/");
+		
+		return view;
+	}
+	
+	@RequestMapping("/delete-product/{productId}")
+	public RedirectView handleDeleteProduct(@PathVariable("productId") int productId, 
+																	   HttpServletRequest request) {
+		productDao.deleteProduct(productId);
+		
+		RedirectView view = new RedirectView();
+		view.setUrl(request.getContextPath() + "/");
+		
+		return view;
 	}
 	
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,46 @@
 <html>
+	<head>
+		<%@ include file="./base.jsp" %>
+		<%@ taglib prefix="c" uri="jakarta.tags.core"  %>
+	</head>
 	<body>
-		<h2>Hello World!</h2>
+		<div class="container mt-3">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="text-center mb-3">Welcome to Product APP</h1>
+					<table class="table table-striped">
+  						<thead>
+    						<tr>
+      							<th scope="col">ID</th>
+      							<th scope="col">Product Name</th>
+      							<th scope="col">Description</th>
+      							<th scope="col">Price</th>
+      							<th scope="col">Action</th>
+    						</tr>
+  						</thead>
+  						<tbody>
+  							<c:forEach items="${products }" var="p">
+    							<tr>
+      								<th scope="row">PROD${p.productId }</th>
+      								<td>${p.name }</td>
+      								<td>${p.description }</td>
+      								<td class="font-weight-bold">&#8377; ${p.price }</td>
+      								<td>
+      									<a href="delete-product/${p.productId }">
+      										<i class="fa-solid fa-trash text-danger"></i>
+      									</a>
+      								</td>
+    							</tr>
+    						</c:forEach>
+  						</tbody>
+					</table>
+					
+					<div class="container text-center">
+						<a href="add-product" class="btn btn-outline-success">Add Product</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
